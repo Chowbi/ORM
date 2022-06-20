e0a6931d1cd2b7a8f752a04ed1f11a466a6e4dfb
Benoit Hoppenot <bhoppenot@oceanet-technology.com>
2022-06-21 00:55:53 +0200

    initial commit

---
+++ DBConnectionInfo.cs
@@ -0,0 +1,25 @@
+﻿namespace ORM
+{
+
+    public enum DbEngine
+    {
+        MySql,
+        MsSql,
+        PSql
+    }
+    public class DBConnectionInfo
+    {
+        public string ConnectionString { get; }
+        public DbEngine Engine { get; set; }
+        public string? ServerName { get; set; }
+        public string? InitialCatalog { get; set; }
+
+        public DBConnectionInfo(DbEngine engine, string connectionString, string? serverName = null, string? initialCatalog = null)
+        {
+            ConnectionString = connectionString;
+            Engine = engine;
+            ServerName = serverName;
+            InitialCatalog = initialCatalog;
+        }
+    }
+}
